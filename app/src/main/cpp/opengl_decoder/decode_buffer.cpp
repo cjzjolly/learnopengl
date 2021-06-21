d67a4ef6874fb7ca536779554a293687d8587f4a
jiezhuchen <jiezhuchen@tencent.com>
2021-06-21 21:47:12 +0800

    存个草稿回去继续

---
+++ app/src/main/cpp/opengl_decoder/decode_buffer.cpp
@@ -0,0 +1,12 @@
+//
+// Created by jiezhuchen on 2021/6/21.
+//
+
+#include <GLES2/gl2.h>
+#include <GLES2/gl2ext.h>
+#include <EGL/egl.h>
+#include <EGL/eglext.h>
+#include <android/native_window.h>
+#include <ui/GraphicBuffer.h>
+#include <dlfcn.h>
+#include "decode_buffer.h"
---
+++ app/src/main/cpp/opengl_decoder/decode_buffer.h
@@ -0,0 +1,14 @@
+//
+// Created by jiezhuchen on 2021/6/21.
+//
+
+#ifndef LEARNOPENGL_DECODE_BUFFER_H
+#define LEARNOPENGL_DECODE_BUFFER_H
+
+
+class decode_buffer {
+
+};
+
+
+#endif //LEARNOPENGL_DECODE_BUFFER_H
