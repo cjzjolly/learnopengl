ea75a1e1d319aa3ce2747324dc3def7ac7c88a3b
jiezhuchen <jiezhuchen@tencent.com>
2021-06-24 16:03:14 +0800

    加入ffmpeg prebuilt 库

---
+++ app/src/main/cpp/ffmpeg/ffmpeg.c
@@ -0,0 +1,42 @@
+//
+// Created by jiezhuchen on 2021/5/19.
+//
+
+#include "stdio.h"
+#include "stdlib.h"
+
+#include <string.h>
+#include <jni.h>
+
+#include "android/log.h"
+
+
+static const char *TAG = "ffmpeg";
+#define LOGI(fmt, args...) __android_log_print(ANDROID_LOG_INFO,  TAG, fmt, ##args)
+#define LOGD(fmt, args...) __android_log_print(ANDROID_LOG_DEBUG, TAG, fmt, ##args)
+#define LOGE(fmt, args...) __android_log_print(ANDROID_LOG_ERROR, TAG, fmt, ##args)
+
+
+char *jstringToChar(JNIEnv *env, jstring jstr);
+
+// 由于jvm和c++对中文的编码不一样，因此需要转码。 utf8/16转换成gb2312
+char *jstringToChar(JNIEnv *env, jstring jstr) {
+    char *rtn = NULL;
+    jclass clsstring = (*env)->FindClass(env, "java/lang/String");
+    jstring strencode = (*env)->NewStringUTF(env, "GB2312");
+    jmethodID mid = (*env)->GetMethodID(env, clsstring, "getBytes", "(Ljava/lang/String;)[B");
+    jbyteArray barr = (jbyteArray) (*env)->CallObjectMethod(env, jstr, mid, strencode);
+    jsize alen = (*env)->GetArrayLength(env, barr);
+    jbyte *ba = (*env)->GetByteArrayElements(env, barr, JNI_FALSE);
+    if (alen > 0) {
+        rtn = (char *) malloc(alen + 1);
+        memcpy(rtn, ba, alen);
+        rtn[alen] = 0;
+    }
+    (*env)->ReleaseByteArrayElements(env, barr, ba, 0);
+    return rtn;
+}
+
+JNIEXPORT void JNICALL Java_com_ffmpeg_test(JNIEnv *env, jobject activity) {
+
+}
