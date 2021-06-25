937dcee1581bae5a1651af224c088f73fe96c67f
jiezhuchen <jiezhuchen@tencent.com>
2021-06-25 19:33:28 +0800

    读写等权限申请，但ffmpeg解帧demo还没成功

--- app/src/main/cpp/ffmpeg/ffmpeg.c
+++ app/src/main/cpp/ffmpeg/ffmpeg.c
@@ -7,8 +7,9 @@
 
 #include <string.h>
 #include <jni.h>
-//#include "arm64-v8a/include/libavcodec/avcodec.h"
 #include "include/libavcodec/avcodec.h"
+#include "include/libavformat/avformat.h"
+#include "include/libswscale/swscale.h"
 #include "android/log.h"
 
 
@@ -17,6 +18,8 @@ static const char *TAG = "ffmpeg";
 #define LOGD(fmt, args...) __android_log_print(ANDROID_LOG_DEBUG, TAG, fmt, ##args)
 #define LOGE(fmt, args...) __android_log_print(ANDROID_LOG_ERROR, TAG, fmt, ##args)
 
+//全局变量区：
+AVFormatContext *mFormatCtx = NULL;
 
 char *jstringToChar(JNIEnv *env, jstring jstr);
 
@@ -38,7 +41,111 @@ char *jstringToChar(JNIEnv *env, jstring jstr) {
     return rtn;
 }
 
+//https://blog.csdn.net/leixiaohua1020/article/details/8652605
+//https://blog.csdn.net/leixiaohua1020/article/details/42181571
+/**初始化FFMPEG**/
+void initFFMpeg() {
+    //FFmpeg
+    struct SwsContext *img_convert_ctx;
+    av_register_all();
+//    avformat_network_init();
+
+    mFormatCtx = avformat_alloc_context();
+}
+
+/**播放文件**/
+int openFile(char *path) {
+    int i, videoindex = -1;
+    AVCodecContext *pCodecCtx;
+    AVCodec *pCodec;
+    AVFrame *pFrame, *pFrameYUV;
+    AVPacket packet;
+    AVCodecParserContext *pCodecParserCtx = NULL;
+    int got_picture;
+    if (mFormatCtx == NULL) {
+        return -1;
+    }
+    if (avformat_open_input(&mFormatCtx, path, NULL, NULL) != 0) {
+        LOGI("Couldn't open input stream.\n");
+        return -1;
+    }
+//    if (avformat_find_stream_info(path, NULL) < 0) {
+//        LOGI("Couldn't find stream information.\n");
+//        return -1;
+//    }
+    //寻找第一个视频帧:
+    for (i = 0; i < mFormatCtx->nb_streams; i++)
+        if (mFormatCtx->streams[i]->codec->codec_type == AVMEDIA_TYPE_VIDEO) {
+            videoindex = i;
+            break;
+        }
+    if (videoindex == -1) {
+        LOGI("Didn't find a video stream.\n");
+        return -1;
+    }
+    //寻找视频流对应的解码器：
+    pCodecCtx = mFormatCtx->streams[videoindex]->codec;
+    //avcodec_register_all();
+    pCodec = avcodec_find_decoder(pCodecCtx->codec_id);
+    if (pCodec == NULL) {
+        LOGI("Codec not found.\n");
+        return -1;
+    }
+    if (avcodec_open2(pCodecCtx, pCodec, NULL) < 0) {
+        LOGI("Could not open codec.\n");
+        return -1;
+    }
+    //获取第一帧的相关信息
+    pCodecParserCtx = av_parser_init(pCodecCtx->codec_id);
+    //解码第一帧:
+    switch (pCodecParserCtx->pict_type) {
+        case AV_PICTURE_TYPE_I:
+            LOGI("Type:I\t");
+            break;
+        case AV_PICTURE_TYPE_P:
+            LOGI("Type:P\t");
+            break;
+        case AV_PICTURE_TYPE_B:
+            LOGI("Type:B\t");
+            break;
+        default:
+            LOGI("Type:Other\t");
+            break;
+    }
+    pFrame = av_frame_alloc();
+    av_init_packet(&packet);
+    LOGI("duration:%d", packet.duration);
+//    int ret = avcodec_decode_video2(pCodecCtx, pFrame, &got_picture, &packet);
+    int ret = avcodec_receive_frame(pCodecCtx, pFrame);
+    if (ret < 0) {
+        LOGI("Decode Error:%d.\n", ret);
+        return ret;
+    }
+    //如果顺利解出了帧
+    if (got_picture) {
+        //Y, U, V
+        for (int i = 0; i < pFrame->height; i++) {
+
+        }
+        for (int i = 0; i < pFrame->height / 2; i++) {
+
+        }
+        for (int i = 0; i < pFrame->height / 2; i++) {
+
+        }
+
+        LOGI("Succeed to decode 1 frame!\n");
+    }
+}
+
+
 JNIEXPORT void JNICALL Java_com_ffmpeg_FFMpegUtil_version(JNIEnv *env, jobject activity) {
     unsigned int ver = avcodec_version();
+    initFFMpeg();
+//    int ret = fopen("/sdcard/Download/bAeqdUAbDOQA.mp4", "r");
+//    LOGI("shit:%d", ret);
+//    fclose(ret);
+//    openFile("/sdcard/Download/bAeqdUAbDOQA.mp4");
+    openFile("/sdcard/DCIM/Camera/VID_20170202_084126.mp4");
     LOGI("cjztest: version:%d", ver);
 }
--- app/src/main/java/com/opengldecoder/jnibridge/MainActivity.java
+++ app/src/main/java/com/opengldecoder/jnibridge/MainActivity.java
@@ -1,6 +1,9 @@
 package com.opengldecoder.jnibridge;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 
@@ -13,8 +16,21 @@ public class MainActivity extends Activity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //分配权限
+        String[] permissions = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO};
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && checkSelfPermission(permissions[0]) != PackageManager.PERMISSION_GRANTED
+                && checkSelfPermission(permissions[1]) != PackageManager.PERMISSION_GRANTED) {
+            requestPermission(permissions);
+        }
         setContentView(new NativeGLSurfaceView(this));
         FFMpegUtil.version();
+    }
+
+    protected void requestPermission(String permissions[]) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(permissions, 1234);
+        }
     }
 
     @Override
