e7c235bdf2416da59f4df769aa3a99fa702b24a2
jiezhuchen <jiezhuchen@tencent.com>
2021-07-02 20:40:25 +0800

    存个草稿回去继续

--- app/src/main/cpp/opengl_decoder/RenderProgram.h
+++ app/src/main/cpp/opengl_decoder/RenderProgram.h
@@ -13,11 +13,21 @@ namespace OPENGL_VIDEO_RENDERER {
         /**创建对象，编译shader，获取shader属性等**/
         virtual void createRender() = 0;
 
-        /**申请渲染
+        /**更新显存中对应纹理地址的图像数据
          * @param outputFBOTexturePointer 最终结果承载FBO，例如图层FBO。各个图层的FBO自底向上渲染
-         * @param texturesPointer 传入已通过texImage2D或PBO放入数据的textures，渲染时映射到shader uniform sample2d对象上**/
-        virtual void requestRender(int outputFBOTexturePointer, int texturesPointer[], int dataArrayLength) = 0;
+         **/
+        virtual void drawData(int outputFBOTexturePointer, char *data, int width, int height, int pixelFormat) = 0;
 
+        /**todo 如果要绘制的东西本身就是一个纹理呢？*
+         * @param outputFBOTexturePointer 最终结果承载FBO，例如图层FBO。各个图层的FBO自底向上渲染*/
+        virtual void drawTexture(int outputFBOTexturePointer, int texturePointer, int width, int height) = 0;
+
+        /**总的资源清理**/
+        virtual void destroy() = 0;
+
+        /**申请渲染
+         * @param outputFBOTexturePointer 最终结果承载FBO，例如图层FBO。各个图层的FBO自底向上渲染**/
+        virtual void requestRender(int outputFBOTexturePointer) = 0;
 
     private:
 
--- app/src/main/cpp/opengl_decoder/RenderProgramYUV.cpp
+++ app/src/main/cpp/opengl_decoder/RenderProgramYUV.cpp
@@ -24,7 +24,37 @@ static const char *TAG = "nativeGL";
 
 using namespace OPENGL_VIDEO_RENDERER;
 
-gLslHandle mProgram;
+gLslHandle mYuvBufferDrawProgram;
+int mGenYTextureId = 0;
+int mGenUVTextureId = 0;
+enum YuvKinds {
+    YUV_420SP_UVUV,
+    YUV_420SP_VUVU,
+    YUV_420P_UUVV,
+    YUV_420P_VVUU,
+};
+/**设定YUV类型**/
+YuvKinds mYuvKinds = YUV_420SP_UVUV;
+int mImgPanelYByteSize;
+int mImgPanelUVByteSize;
+bool mIsFirstFrame = true;
+GLuint mYPanelPixelBuffferPointerArray[2];
+GLuint mUVPanelPixelBuffferPointerArray[2];
+GLint mGLFrameObjectPositionPointer;
+GLint mGLFrameBufferProgramFunChoicePointer;
+GLint mGLFrameVTexCoordPointer;
+GLint mGLFrameObjectVertColorArrayPointer;
+GLint mGLFrameuMVPMatrixPointer;
+GLint mFrameCountPointer;
+GLint mResoulutionPointer;
+float texCoor[]   //纹理内采样坐标,类似于canvas坐标 //这东西有问题，导致两个framebuffer的画面互相取纹理时互为颠倒
+        {
+                1.0, 0.0,
+                0.0, 0.0,
+                1.0, 1.0,
+                0.0, 1.0
+        };
+float mVert[4 * 3];
 
 char vertShader[] = GL_SHADER_STRING(
         version 300 es
@@ -109,14 +139,152 @@ char fragShader[] = GL_SHADER_STRING(
 
 );
 
-
-void RenderProgramYUV::createRender() {
-    mProgram = createProgram(vertShader, fragShader);
+void createEmptyTexture(int *texturePointers, int imgWidth, int imgHeight, int pixelFormat) {
+    glGenTextures(1, (GLuint*) texturePointers); //只要值不重复即可
+    //UV纹理初始化
+    glBindTexture(GL_TEXTURE_2D, texturePointers[0]);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    //创建一个占用指定空间的纹理，但暂时不复制数据进去，等PBO进行数据传输，取代glTexImage2D，利用DMA提高数据拷贝速度
+    glTexImage2D(GL_TEXTURE_2D, 0, pixelFormat, imgWidth, imgHeight, 0, pixelFormat, GL_UNSIGNED_BYTE, nullptr); //因为这里使用了双字节，所以纹理大小对比使用单字节的Y通道纹理，宽度首先要缩小一般，而uv层高度本来就只有y层一般，所以高度也除以2
 }
 
-void RenderProgramYUV::requestRender(int outputFBOTexturePointer, int *texturesPointer,
-                                     int dataArrayLength) {
+/**
+ * 创建2个PBO提高纹理更新的速度
+ **/
+void createPBO(int imgWidth, int imgHeight) {
+    //创建Y通道PBO
+    mImgPanelYByteSize = imgWidth * imgHeight;
+    glGenBuffers(2, mYPanelPixelBuffferPointerArray);
 
+    glBindBuffer(GL_PIXEL_UNPACK_BUFFER, mYPanelPixelBuffferPointerArray[0]);
+    glBufferData(GL_PIXEL_UNPACK_BUFFER, mImgPanelYByteSize, nullptr, GL_STREAM_DRAW);
+
+    glBindBuffer(GL_PIXEL_PACK_BUFFER, mYPanelPixelBuffferPointerArray[1]);
+    glBufferData(GL_PIXEL_PACK_BUFFER, mImgPanelYByteSize,  nullptr, GL_STREAM_DRAW);
+    //创建UV通道PBO
+    mImgPanelUVByteSize = imgWidth * imgHeight / 2;
+    mUVPanelPixelBuffferPointerArray = new int[2];
+    glGenBuffers(2, mUVPanelPixelBuffferPointerArray);
+
+    glBindBuffer(GL_PIXEL_UNPACK_BUFFER, mUVPanelPixelBuffferPointerArray[0]);
+    glBufferData(GL_PIXEL_UNPACK_BUFFER, mImgPanelUVByteSize,  nullptr, GL_STREAM_DRAW);
+
+    glBindBuffer(GL_PIXEL_PACK_BUFFER, mUVPanelPixelBuffferPointerArray[1]);
+    glBufferData(GL_PIXEL_PACK_BUFFER, mImgPanelUVByteSize,  nullptr, GL_STREAM_DRAW);
+    //初始化完重新绑定默认buffer，否则可能影响其他图形
+    glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0);
+    glBindBuffer(GL_PIXEL_PACK_BUFFER, 0);
+}
+
+void RenderProgramYUV::createRender() {
+    mYuvBufferDrawProgram = createProgram(vertShader, fragShader);
+    mYuvKinds = YUV_420SP_UVUV;
+
+    //获取程序中顶点位置属性引用"指针"
+    mGLFrameObjectPositionPointer = glGetAttribLocation(mYuvBufferDrawProgram.programHandle, "objectPosition");
+    //渲染方式选择
+    mGLFrameBufferProgramFunChoicePointer = glGetUniformLocation(mYuvBufferDrawProgram.programHandle, "funChoice");
+    //纹理采样坐标
+    mGLFrameVTexCoordPointer = glGetAttribLocation(mYuvBufferDrawProgram.programHandle, "vTexCoord");
+    //获取程序中顶点颜色属性引用"指针"
+    mGLFrameObjectVertColorArrayPointer = glGetAttribLocation(mYuvBufferDrawProgram.programHandle, "objectColor");
+    //获取程序中总变换矩阵引用"指针"
+    mGLFrameuMVPMatrixPointer = glGetUniformLocation(mYuvBufferDrawProgram.programHandle, "uMVPMatrix");
+    //渲染帧计数指针
+    mFrameCountPointer = glGetUniformLocation(mYuvBufferDrawProgram.programHandle, "frame");
+    //设置分辨率指针，告诉gl脚本现在的分辨率
+    mResoulutionPointer = glGetUniformLocation(mYuvBufferDrawProgram.programHandle, "resolution");
+
+
+}
+
+void refreshBuffer(char *imgBytes, int mImgWidth, int mImgHeight) {
+    //更新ypanel数据
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, mGenYTextureId);
+    glUniform1i(glGetUniformLocation(mYuvBufferDrawProgram.programHandle, "textureY"), 0); //获取纹理属性的指针
+    glBindBuffer(GL_PIXEL_UNPACK_BUFFER, mYPanelPixelBuffferPointerArray[mFrameCount % 2]);
+    glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, mImgWidth, mImgHeight, GL_LUMINANCE, GL_UNSIGNED_BYTE, null); //1字节为一个单位
+    //更新图像数据，复制到 PBO 中
+    glBindBuffer(GL_PIXEL_UNPACK_BUFFER, mYPanelPixelBuffferPointerArray[(mFrameCount + 1) % 2]);
+    glBufferData(GL_PIXEL_UNPACK_BUFFER, mImgPanelYByteSize, null, GL_STREAM_DRAW);
+    Buffer buf = glMapBufferRange(GL_PIXEL_UNPACK_BUFFER, 0, mImgPanelYByteSize, GL_MAP_WRITE_BIT | GL_MAP_INVALIDATE_BUFFER_BIT);
+    //填充像素
+    ByteBuffer bytebuffer = ((ByteBuffer) buf).order(ByteOrder.nativeOrder());
+    bytebuffer.position(0);
+    bytebuffer.put(imgBytes, 0, mImgWidth * mImgHeight);
+    bytebuffer.position(0);
+    glUnmapBuffer(GL_PIXEL_UNPACK_BUFFER);
+    glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0);
+    //更新uvpanel数据
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, mGenUVTextureId);
+    glUniform1i(glGetUniformLocation(mYuvBufferDrawProgram.programHandle, "textureUV"), 1); //获取纹理属性的指针
+    glBindBuffer(GL_PIXEL_UNPACK_BUFFER, mUVPanelPixelBuffferPointerArray[mFrameCount % 2]);
+    switch (mYuvKinds) {
+        default:
+        case YUV_420SP_UVUV:
+        case YUV_420SP_VUVU:
+            glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, mImgWidth / 2, mImgHeight / 2, GL_LUMINANCE_ALPHA, GL_UNSIGNED_BYTE, null); //2字节为一个单位，所以宽度因为单位为2字节一个，对比1字节时直接对半
+            break;
+        case YUV_420P_UUVV:
+        case YUV_420P_VVUU:
+            glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, mImgWidth, mImgHeight / 2, GL_LUMINANCE, GL_UNSIGNED_BYTE, null);
+            break;
+    }
+    //更新图像数据，复制到 PBO 中
+    glBindBuffer(GL_PIXEL_UNPACK_BUFFER, mUVPanelPixelBuffferPointerArray[(mFrameCount + 1) % 2]);
+    glBufferData(GL_PIXEL_UNPACK_BUFFER, mImgPanelUVByteSize, null, GL_STREAM_DRAW);
+    buf = glMapBufferRange(GL_PIXEL_UNPACK_BUFFER, 0, mImgPanelUVByteSize, GL_MAP_WRITE_BIT | GL_MAP_INVALIDATE_BUFFER_BIT);
+    //填充像素
+    bytebuffer = ((ByteBuffer) buf).order(ByteOrder.nativeOrder());
+    bytebuffer.position(0);
+    bytebuffer.put(imgBytes, mImgWidth * mImgHeight, mImgWidth * mImgHeight / 2);
+    bytebuffer.position(0);
+    glUnmapBuffer(GL_PIXEL_UNPACK_BUFFER);
+    glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0);
+}
+
+void drawTo(int fboTexturePointer, float *cameraMatrix, float *projMatrix) {
+    if (mIsDestroyed) {
+        return;
+    }
+    locationTrans(cameraMatrix, projMatrix, mGLFrameuMVPMatrixPointer);
+    //先绘制内容到frambuffer
+    drawToFrameBuffer(cameraMatrix, projMatrix);
+    //绘制
+    glUseProgram(mYuvBufferDrawProgram.programHandle);
+    locationTrans(cameraMatrix, projMatrix, mGLFrameuMVPMatrixPointer);
+}
+
+/**YUV格式如果使用opengles 3.0的话，不需要在此时使用glTexImage2d，更建议在更新纹理的接口中使用PBO更新**/
+void RenderProgramYUV::drawData(int outputFBOTexturePointer, char *data, int width, int height, int pixelFormat) {
+    if (mIsFirstFrame) {
+        //生成textureY纹理
+        int mGenYTexutreArray[1];
+        createEmptyTexture(mGenYTexutreArray, width, width, GL_LUMINANCE);
+        mGenYTextureId = mGenYTexutreArray[0];
+        //生成textureUV纹理
+        int mGenUVTexutreArray[1];
+        switch (mYuvKinds) {
+            default:
+            case YUV_420SP_UVUV:
+            case YUV_420SP_VUVU:
+                createEmptyTexture(mGenUVTexutreArray, width / 2, height / 2, GL_LUMINANCE_ALPHA);
+                break;
+            case YUV_420P_UUVV:
+            case YUV_420P_VVUU:
+                createEmptyTexture(mGenUVTexutreArray, width, height / 2, GL_LUMINANCE);
+                break;
+        }
+        mGenUVTextureId = mGenUVTexutreArray[0];
+        createPBO(width, height);
+    }
+    refreshBuffer(data, width, height);
+    mIsFirstFrame = false;
 }
 
 extern "C" {
