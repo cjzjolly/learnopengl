110953a04317dbdcbe29ff99bfd3158a8c6cd9e6
jiezhuchen <jiezhuchen@tencent.com>
2021-08-18 14:50:58 +0800

    GLCurverPath又写了一丢丢

--- app/src/main/java/com/cjztest/glShaderEffect/GLCurvePath.java
+++ app/src/main/java/com/cjztest/glShaderEffect/GLCurvePath.java
@@ -1,10 +1,12 @@
 package com.cjztest.glShaderEffect;
 
+import android.content.Context;
 import android.graphics.Point;
 import android.graphics.PointF;
 import android.view.MotionEvent;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 /**曲线实现**/
@@ -13,8 +15,18 @@ public class GLCurvePath extends GLObject {
     private List<PointF> mVertx = new ArrayList<>();
     //线条最小宽度
     private float mMinCurveWidth = 0.01f;
+    private int mPointCount = 0;
     private PointF mPrevTouchPoint = null;
+    private int mContainerW;
+    private int mContainerH;
+    private int mPaintWidth;
+    private List<PointF> mGLPoints = new LinkedList<>();
 
+    public GLCurvePath(int windowW, int windowH, int paintWidth) {
+        this.mContainerW = windowW;
+        this.mContainerH = windowH;
+        this.mPaintWidth = paintWidth;
+    }
 
     public void onTouch(MotionEvent event) {
        switch (event.getAction()) {
@@ -22,6 +34,7 @@ public class GLCurvePath extends GLObject {
                 mPrevTouchPoint = new PointF();
                 mPrevTouchPoint.x = event.getX();
                 mPrevTouchPoint.y = event.getY();
+                mPointCount ++;
                 break;
             case MotionEvent.ACTION_MOVE:
                 mPrevTouchPoint.x = event.getX();
@@ -30,6 +43,11 @@ public class GLCurvePath extends GLObject {
                 *  1、先确定两点连线的旋转角度a
                 *  2、把指定宽度作为长度的线条旋转a + 90度
                 *  3、step2的线条的中点移动到step1线段端点处*/
+                mPointCount ++;
+                if (mPointCount > 1) {
+                    //把容器得到的触摸坐标，对换成OpenGL线条对应位置的浮点坐标:
+
+                }
                 break;
             case MotionEvent.ACTION_UP:
                 break;
