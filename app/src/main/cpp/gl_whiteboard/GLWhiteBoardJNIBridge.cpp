fadd300723894957d81741235348079909445cc8
chenjiezhu <chenjiezhu@wps.cn>
2022-09-14 16:07:36 +0800

    暂时注释用不上的代码

--- app/src/main/cpp/gl_whiteboard/GLWhiteBoardJNIBridge.cpp
+++ app/src/main/cpp/gl_whiteboard/GLWhiteBoardJNIBridge.cpp
@@ -1,64 +1,64 @@
+////
+//// Created by jiezhuchen on 22-4-19.
+////
+//#include "stdio.h"
+//#include "stdlib.h"
 //
-// Created by jiezhuchen on 22-4-19.
+//#include <string.h>
+//#include <jni.h>
 //
-#include "stdio.h"
-#include "stdlib.h"
-
-#include <string.h>
-#include <jni.h>
-
-#include <GLES3/gl3.h>
-#include <GLES3/gl3ext.h>
-
-#include "android/log.h"
-#include "android/bitmap.h"
-#include "matrix.h"
-#include "GLWhiteBoardJNIBridge.h"
-
-static const char *TAG = "nativeGL";
-#define LOGI(fmt, args...) __android_log_print(ANDROID_LOG_INFO,  TAG, fmt, ##args)
-#define LOGD(fmt, args...) __android_log_print(ANDROID_LOG_DEBUG, TAG, fmt, ##args)
-#define LOGE(fmt, args...) __android_log_print(ANDROID_LOG_ERROR, TAG, fmt, ##args)
-
-extern "C" {
-    float mRatio;
-    int mWidth;
-    int mHeight;
-    float mProjMatrix[16];
-    float mCameraMatrix[16];
-
-    /**openGL初始化**/
-    void setupGraphics(int w, int h) {
-        glViewport(0, 0, w, h);//设置视口
-        float ratio = (float) h / w;//计算宽长比glViewport
-        mWidth = w;
-        mHeight = h;
-        mRatio = ratio;
-        frustumM(mProjMatrix, 0, -1, 1, -ratio, ratio, 1, 550);//设置投影矩阵
-        setLookAtM(mCameraMatrix, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0);//设置摄像机矩阵
-        glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT); //清理屏幕
-        glClearColor(0, 0, 0, 0);//设置背景颜色
-        glEnable(GL_DEPTH_TEST);
-        glDepthFunc(GL_LEQUAL); //还可以
-        //开启透明度混合能力
-        glEnable(GL_BLEND);
-        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);  //todo 这个混合导致光影效果有点问题，需要处理一下
-        glDisable(GL_DITHER);
-        return;
-    }
-
-    /**根据触摸信息绘制线条*/
-    JNIEXPORT void JNICALL
-    Java_com_whiteboard_JniBridge_touchAndDraw(JNIEnv *env, jobject activity,
-                                               jfloat x, jfloat y, jint action) {
-
-    }
-
-    /**安卓系统初始化EGL等，但分情况，安卓系统在有GLSurfaceview的情况下不需要进行EGL相关操作**/
-    JNIEXPORT void JNICALL
-    Java_com_whiteboard_JniBridge_nativeGLInit(JNIEnv *env, jobject activity,
-                                                            jint viewPortWidth, jint viewPortHeight) {
-        setupGraphics(viewPortWidth, viewPortHeight);
-        LOGI("cjztest Java_com_opengldecoder_jnibridge_JniBridge_nativeGLInit, width:%d, height:%d", viewPortWidth, viewPortHeight);
-    }
-}
+//#include <GLES3/gl3.h>
+//#include <GLES3/gl3ext.h>
+//
+//#include "android/log.h"
+//#include "android/bitmap.h"
+//#include "matrix.h"
+//#include "GLWhiteBoardJNIBridge.h"
+//
+//static const char *TAG = "nativeGL";
+//#define LOGI(fmt, args...) __android_log_print(ANDROID_LOG_INFO,  TAG, fmt, ##args)
+//#define LOGD(fmt, args...) __android_log_print(ANDROID_LOG_DEBUG, TAG, fmt, ##args)
+//#define LOGE(fmt, args...) __android_log_print(ANDROID_LOG_ERROR, TAG, fmt, ##args)
+//
+//extern "C" {
+//    float mRatio;
+//    int mWidth;
+//    int mHeight;
+//    float mProjMatrix[16];
+//    float mCameraMatrix[16];
+//
+//    /**openGL初始化**/
+//    void setupGraphics(int w, int h) {
+//        glViewport(0, 0, w, h);//设置视口
+//        float ratio = (float) h / w;//计算宽长比glViewport
+//        mWidth = w;
+//        mHeight = h;
+//        mRatio = ratio;
+//        frustumM(mProjMatrix, 0, -1, 1, -ratio, ratio, 1, 550);//设置投影矩阵
+//        setLookAtM(mCameraMatrix, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0);//设置摄像机矩阵
+//        glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT); //清理屏幕
+//        glClearColor(0, 0, 0, 0);//设置背景颜色
+//        glEnable(GL_DEPTH_TEST);
+//        glDepthFunc(GL_LEQUAL); //还可以
+//        //开启透明度混合能力
+//        glEnable(GL_BLEND);
+//        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);  //todo 这个混合导致光影效果有点问题，需要处理一下
+//        glDisable(GL_DITHER);
+//        return;
+//    }
+//
+//    /**根据触摸信息绘制线条*/
+//    JNIEXPORT void JNICALL
+//    Java_com_whiteboard_JniBridge_touchAndDraw(JNIEnv *env, jobject activity,
+//                                               jfloat x, jfloat y, jint action) {
+//
+//    }
+//
+//    /**安卓系统初始化EGL等，但分情况，安卓系统在有GLSurfaceview的情况下不需要进行EGL相关操作**/
+//    JNIEXPORT void JNICALL
+//    Java_com_whiteboard_JniBridge_nativeGLInit(JNIEnv *env, jobject activity,
+//                                                            jint viewPortWidth, jint viewPortHeight) {
+//        setupGraphics(viewPortWidth, viewPortHeight);
+//        LOGI("cjztest Java_com_opengldecoder_jnibridge_JniBridge_nativeGLInit, width:%d, height:%d", viewPortWidth, viewPortHeight);
+//    }
+//}
