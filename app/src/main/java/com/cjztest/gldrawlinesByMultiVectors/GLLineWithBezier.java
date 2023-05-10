49d56986b291a26bb0edc427ab2ebfb6e48be48f
chenjiezhu <chenjiezhu@wps.cn>
2023-05-10 21:12:11 +0800

    暂存进度，以后要把冗余代码处理一下。

--- app/src/main/java/com/cjztest/gldrawlinesByMultiVectors/GLLineWithBezier.java
+++ app/src/main/java/com/cjztest/gldrawlinesByMultiVectors/GLLineWithBezier.java
@@ -17,18 +17,34 @@ public class GLLineWithBezier {
     private ByteBuffer mPointByteBuffer;
     /**顶点RGBA字节数组**/
     private ByteBuffer mColorByteBuffer;
+    /**线头顶点字节数组**/
+    private ByteBuffer mHeadCapPointByteBuffer;
+    /**线头顶点RGBA字节数组**/
+    private ByteBuffer mHeadCapColorByteBuffer;
     /**顶点坐标数组**/
     private FloatBuffer mPointBuf = null;
     /**顶点RGBA数组**/
     private FloatBuffer mColorBuf = null;
+    /**线头顶点坐标数组**/
+    private FloatBuffer mHeadPointBuf = null;
+    /**线头顶点RGBA数组**/
+    private FloatBuffer mHeadColorBuf = null;
     /**正在写入第几个顶点float**/
     private int mPointBufferPos = 0;
     /**正在写入第几个颜色float**/
     private int mColorBufferPos = 0;
+    /**HeadCap正在写入第几个顶点float**/
+    private int mHeadCapPointBufferPos = 0;
+    /**HeadCap正在写入第几个颜色float**/
+    private int mHeadCapColorBufferPos = 0;
     /**初始化时的顶点数目**/
     private int mInitVertexCount = 12;
     /**初始化时的颜色单元数目**/
     private int mInitColorCount = 16;
+    /**HeadCap初始化时的顶点数目**/
+    private int mHeadInitVertexCount = 12;
+    /**HeadCap初始化时的颜色单元数目**/
+    private int mHeadInitColorCount = 16;
 
     /**线条宽度**/
 //    private float mLineWidth = 0.05f;
@@ -83,10 +99,25 @@ public class GLLineWithBezier {
         return rotatedVec;
     }
 
-    /**todo 绘制线头 bug 现在这套只能做到GL_LINE_FAN绘制，这样会引起一些问题**/
+    /**todo 绘制线头 bug 现在这套只能做到GL_LINE_FAN绘制，这样会引起一些问题，感觉还是分解成几个部分绘制更好**/
     private void lineCap(@NonNull float firstVec[], @NonNull float secVec[]) {
         if (null == firstVec) {
             return;
+        }
+        if (mHeadPointBuf == null) {
+            mHeadCapPointByteBuffer = ByteBuffer.allocateDirect(mHeadInitVertexCount * 4);    //顶点数 * sizeof(float)
+            mHeadCapPointByteBuffer.order(ByteOrder.nativeOrder());
+            mHeadPointBuf = mHeadCapPointByteBuffer.asFloatBuffer();
+            mHeadPointBuf.position(0);
+            mHeadCapPointBufferPos = 0;
+        }
+        //按初始化大小初始化RGBA字节数组和RGBA数组
+        if (mHeadColorBuf == null) {
+            mHeadCapColorByteBuffer = ByteBuffer.allocateDirect(mHeadInitColorCount * 4);
+            mHeadCapColorByteBuffer.order(ByteOrder.nativeOrder());
+            mHeadColorBuf = mHeadCapColorByteBuffer.asFloatBuffer();
+            mHeadColorBuf.position(0);
+            mHeadCapColorBufferPos = 0;
         }
         /**1、了解线条开始的方向，将半径线条绕旋转该方向与标准测量用向量的夹角的角度量
          * 2、旋转180度时按照一定步进产生多个顶点，todo 但怎么确定旋转的方向是顺时针还是逆时针？以什么为依据判断？以传入向量方向为参考，但具体怎么做？*/
@@ -111,52 +142,22 @@ public class GLLineWithBezier {
         List<float[]> newVecs = new LinkedList<>();
         for (double degreeBias = 180 + angle; degreeBias >= 0 + angle; degreeBias -= step) {
             try {
-                if (degreeBias > 0 + angle) {
-                    float rotatedVec[] = rotate2d(initVert, degreeBias);
-                    float newVec[] = new float[6];
-                    //偏移到对应位置
-                    newVec[0] = rotatedVec[0] + firstVec[0];
-                    newVec[1] = rotatedVec[1] + firstVec[1];
-                    newVec[3] += firstVec[0];
-                    newVec[4] += firstVec[1];
-                    newVecs.add(newVec);
-                } else {
-                    float vert[] = new float[] {
-                            mLineWidth / 2f, 0
-                    };
-                    float rotatedVec[] = rotate2d(initVert, degreeBias);
-                    float rotatedVec2[] = rotate2d(vert, degreeBias);
-                    float newVec[] = new float[6];
-                    //偏移到对应位置
-                    newVec[0] = rotatedVec[0] + firstVec[0];
-                    newVec[1] = rotatedVec[1] + firstVec[1];
-                    newVec[3] = rotatedVec2[0] + firstVec[0];
-                    newVec[4] = rotatedVec2[1] + firstVec[1];
-                    newVecs.add(newVec);
-                }
+                float rotatedVec[] = rotate2d(initVert, degreeBias);
+                float newVec[] = new float[6];
+                //偏移到对应位置
+                newVec[0] = rotatedVec[0] + firstVec[0];
+                newVec[1] = rotatedVec[1] + firstVec[1];
+                newVec[3] += firstVec[0];
+                newVec[4] += firstVec[1];
+                newVecs.add(newVec);
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
 
         for (float[] newVec : newVecs) {
-            if (mPointBuf == null) {
-                mPointByteBuffer = ByteBuffer.allocateDirect(mInitVertexCount * 4);    //顶点数 * sizeof(float)
-                mPointByteBuffer.order(ByteOrder.nativeOrder());
-                mPointBuf = mPointByteBuffer.asFloatBuffer();
-                mPointBuf.position(0);
-                mPointBufferPos = 0;
-            }
-            //按初始化大小初始化RGBA字节数组和RGBA数组
-            if (mColorBuf == null) {
-                mColorByteBuffer = ByteBuffer.allocateDirect(mInitColorCount * 4);
-                mColorByteBuffer.order(ByteOrder.nativeOrder());
-                mColorBuf = mColorByteBuffer.asFloatBuffer();
-                mColorBuf.position(0);
-                mColorBufferPos = 0;
-            }
             for (int i = 0; i < newVec.length; i++) {
-                mPointBuf.put(mPointBufferPos++, newVec[i]);
+                mHeadPointBuf.put(mHeadCapPointBufferPos++, newVec[i]);
             }
             for (int i = 0; i < newVec.length / 3; i++) {
                 //写入颜色值r,g,b,a
@@ -165,10 +166,10 @@ public class GLLineWithBezier {
                 float blue = (float) ((color & 0x000000FF)) / 255f;
                 float green = (float) ((color & 0x0000FF00) >> 8) / 255f;
                 float red = (float) ((color & 0x00FF0000) >> 16) / 255f;
-                mColorBuf.put(mColorBufferPos++, red);
-                mColorBuf.put(mColorBufferPos++, green);
-                mColorBuf.put(mColorBufferPos++, blue);
-                mColorBuf.put(mColorBufferPos++, alpha);
+                mHeadColorBuf.put(mHeadCapColorBufferPos++, red);
+                mHeadColorBuf.put(mHeadCapColorBufferPos++, green);
+                mHeadColorBuf.put(mHeadCapColorBufferPos++, blue);
+                mHeadColorBuf.put(mHeadCapColorBufferPos++, alpha);
             }
             checkCapacity();
         }
@@ -311,6 +312,25 @@ public class GLLineWithBezier {
             mColorBuf = mColorByteBuffer.asFloatBuffer();
             mColorBuf.position(0);
         }
+        if (mHeadCapPointBufferPos >= mHeadInitVertexCount) {
+            Log.i("GLLines", "扩容点数到:" + mHeadInitVertexCount);
+            mHeadInitVertexCount += 12;
+            mHeadInitColorCount += 16;
+
+            ByteBuffer qbb = ByteBuffer.allocateDirect(mHeadInitVertexCount * 4);    //顶点数 * sizeof(float) ; 加4个点
+            qbb.order(ByteOrder.nativeOrder());
+            System.arraycopy(mHeadCapPointByteBuffer.array(), 0, qbb.array(), 0, (mHeadCapPointBufferPos + 1) * 4);   //顶点数 * sizeof(float)
+            mHeadCapPointByteBuffer = qbb;
+            mHeadPointBuf = mHeadCapPointByteBuffer.asFloatBuffer();
+            mHeadPointBuf.position(0);
+
+            ByteBuffer qbb2 = ByteBuffer.allocateDirect(mHeadInitColorCount * 4);    //顶点数 * sizeof(float) ;
+            qbb2.order(ByteOrder.nativeOrder());
+            System.arraycopy(mHeadCapColorByteBuffer.array(), 0, qbb2.array(), 0, (mHeadCapColorBufferPos + 1) * 4);  //sizeof(R,G,B,Alpha) * sizeof(float)
+            mHeadCapColorByteBuffer = qbb2;
+            mHeadColorBuf = mHeadCapColorByteBuffer.asFloatBuffer();
+            mHeadColorBuf.position(0);
+        }
     }
 
     //XY平面上的的旋转量
@@ -348,6 +368,35 @@ public class GLLineWithBezier {
     public void draw(int vertPointer, int colorPointer) {
         FloatBuffer lineVerts = mPointBuf;
         FloatBuffer colors = mColorBuf;
+        if (mHeadPointBuf != null && mHeadColorBuf != null) {
+            mHeadPointBuf.position(0);
+            mHeadColorBuf.position(0);
+            //将顶点位置数据送入渲染管线
+            GLES30.glVertexAttribPointer
+                    (
+                            vertPointer,
+                            3,
+                            GLES30.GL_FLOAT,
+                            false,
+                            0,  //stride是啥？
+                            mHeadPointBuf
+                    );
+            //将顶点颜色数据送入渲染管线
+            GLES30.glVertexAttribPointer
+                    (
+                            colorPointer,
+                            4,
+                            GLES30.GL_FLOAT,
+                            false,
+                            0,
+                            mHeadColorBuf
+                    );
+            GLES30.glEnableVertexAttribArray(vertPointer); //启用顶点属性
+            GLES30.glEnableVertexAttribArray(colorPointer);  //启用颜色属性
+            GLES30.glDrawArrays(GLES30.GL_TRIANGLE_STRIP, 0, mHeadCapPointBufferPos / 3); //cjztest
+            GLES30.glDisableVertexAttribArray(vertPointer); //启用顶点属性
+            GLES30.glDisableVertexAttribArray(colorPointer);  //启用颜色属性
+        }
         if (lineVerts != null && colors != null) {
             lineVerts.position(0);
             colors.position(0);
